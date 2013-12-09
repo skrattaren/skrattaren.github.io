a86f03b4ecdcf2c69f73458d475fa844effa271e
Nikolaj Šujskij <skrattaren@yandex.ru>
2013-12-09 13:42:33 +0400

    Prettify URLs (strip ".html")

--- conf.py
+++ conf.py
@@ -367,7 +367,7 @@ COMMENT_SYSTEM_ID = "skrattaren-bblogg"
 # it will instead /foo/default.html => /foo)
 # (Note: This was briefly STRIP_INDEX_HTML in v 5.4.3 and 5.4.4)
 # Default = False
-# STRIP_INDEXES = False
+STRIP_INDEXES = True
 
 # Should the sitemap list directories which only include other directories
 # and no files.
@@ -382,7 +382,7 @@ COMMENT_SYSTEM_ID = "skrattaren-bblogg"
 # This can be disabled on a per-page/post basis by adding
 #    .. pretty_url: False
 # to the metadata
-# PRETTY_URLS = False
+PRETTY_URLS = True
 
 # If True, publish future dated posts right away instead of scheduling them.
 # Defaults to False.
