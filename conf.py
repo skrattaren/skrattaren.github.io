19ff75ea09febe94744d3e954d8864c53ebb5d20
Nikolaj Šujskij <skrattaren@yandex.ru>
2014-10-12 23:36:01 +0400

    Screw assets

--- conf.py
+++ conf.py
@@ -709,7 +709,7 @@ UNSLUGIFY_TITLES = True
 # }
 
 # If webassets is installed, bundle JS and CSS to make site loading faster
-# USE_BUNDLES = True
+USE_BUNDLES = False
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
