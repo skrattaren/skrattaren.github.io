0dc5acb407ae5abbc07bdf5f2984d4157ec39d96
Nikolaj Šujskij <skrattaren@yandex.ru>
2014-11-01 16:31:23 +0300

    Screw pagination

--- conf.py
+++ conf.py
@@ -590,7 +590,7 @@ SOCIAL_BUTTONS_CODE = ""
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 666
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
