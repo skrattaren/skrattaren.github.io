20bcb31f715b2cf02b22b0986bf9268a9cddedfb
Nikolaj Šujskij <skrattaren@yandex.ru>
2017-10-17 23:44:32 +0300

    Fix name

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Шуйский Николай (Nikolaj Šuskij)"
+BLOG_AUTHOR = "Николай Шуйский (Nikolaj Šujskij)"
 BLOG_TITLE = "Unseen Hexadecimals"  # translatable
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
