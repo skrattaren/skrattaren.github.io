f122450f1faa543220170c868036c1478f486881
Nikolaj Šujskij <skrattaren@yandex.ru>
2022-06-24 23:30:30 +0400

    Fix PAGES path, comment unused POST/PAGES formats

--- conf.py
+++ conf.py
@@ -249,15 +249,15 @@ THEME_CONFIG = {
 
 POSTS = (
     ("posts/*.rst", "posts", "post.tmpl"),
-    ("posts/*.md", "posts", "post.tmpl"),
-    ("posts/*.txt", "posts", "post.tmpl"),
-    ("posts/*.html", "posts", "post.tmpl"),
+    # ("posts/*.md", "posts", "post.tmpl"),
+    # ("posts/*.txt", "posts", "post.tmpl"),
+    # ("posts/*.html", "posts", "post.tmpl"),
 )
 PAGES = (
-    ("pages/*.rst", "pages", "page.tmpl"),
-    ("pages/*.md", "pages", "page.tmpl"),
-    ("pages/*.txt", "pages", "page.tmpl"),
-    ("pages/*.html", "pages", "page.tmpl"),
+    ("pages/*.rst", ".", "page.tmpl"),
+    # ("pages/*.md", "pages", "page.tmpl"),
+    # ("pages/*.txt", "pages", "page.tmpl"),
+    # ("pages/*.html", "pages", "page.tmpl"),
 )
 
 
