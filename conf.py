c5a535114e7843fa9e84b48b46fe666bd115b37c
Nikolaj Šujskij <skrattaren@yandex.ru>
2025-06-10 01:42:16 +0400

    Enable Google Analytics (-<{

--- conf.py
+++ conf.py
@@ -1292,7 +1292,17 @@ INDEX_DISPLAY_POST_COUNT = 13
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-72F84B9WNM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-72F84B9WNM');
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
